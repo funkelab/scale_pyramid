652db5fcc00309a1fe315af458bbd2895a7acf27
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-02-01 16:35:17 +0000

    blacken scale_pyramid

--- scale_pyramid.py
+++ scale_pyramid.py
@@ -6,6 +6,7 @@ import zarr
 
 # monkey-patch os.mkdirs, due to bug in zarr
 import os
+
 prev_makedirs = os.makedirs
 
 
@@ -27,10 +28,10 @@ def downscale_block(in_array, out_array, factor, block):
 
     n_channels = len(in_data.shape) - dims
     if n_channels >= 1:
-        factor = (1,)*n_channels + factor
+        factor = (1,) * n_channels + factor
 
     if in_data.dtype == np.uint64:
-        slices = tuple(slice(k//2, None, k) for k in factor)
+        slices = tuple(slice(k // 2, None, k) for k in factor)
         out_data = in_data[slices]
     else:
         out_data = skimage.measure.block_reduce(in_data, factor, np.mean)
@@ -49,7 +50,7 @@ def downscale(in_array, out_array, factor, write_size):
     print("Downsampling by factor %s" % (factor,))
 
     dims = in_array.roi.dims()
-    block_roi = daisy.Roi((0,)*dims, write_size)
+    block_roi = daisy.Roi((0,) * dims, write_size)
 
     print("Processing ROI %s with blocks %s" % (out_array.roi, block_roi))
 
@@ -57,15 +58,12 @@ def downscale(in_array, out_array, factor, write_size):
         out_array.roi,
         block_roi,
         block_roi,
-        process_function=lambda b: downscale_block(
-            in_array,
-            out_array,
-            factor,
-            b),
+        process_function=lambda b: downscale_block(in_array, out_array, factor, b),
         read_write_conflict=False,
         num_workers=60,
         max_retries=0,
-        fit='shrink')
+        fit="shrink",
+    )
 
 
 def create_scale_pyramid(in_file, in_ds_name, scales, chunk_shape):
@@ -78,13 +76,13 @@ def create_scale_pyramid(in_file, in_ds_name, scales, chunk_shape):
     except Exception:
         raise RuntimeError("%s does not seem to be a dataset" % in_ds_name)
 
-    if not in_ds_name.endswith('/s0'):
+    if not in_ds_name.endswith("/s0"):
 
-        ds_name = in_ds_name + '/s0'
+        ds_name = in_ds_name + "/s0"
 
         print("Moving %s to %s" % (in_ds_name, ds_name))
-        ds.store.rename(in_ds_name, in_ds_name + '__tmp')
-        ds.store.rename(in_ds_name + '__tmp', ds_name)
+        ds.store.rename(in_ds_name, in_ds_name + "__tmp")
+        ds.store.rename(in_ds_name + "__tmp", ds_name)
 
     else:
 
@@ -106,27 +104,24 @@ def create_scale_pyramid(in_file, in_ds_name, scales, chunk_shape):
     elif prev_array.n_channel_dims == 1:
         num_channels = prev_array.shape[0]
     else:
-        raise RuntimeError(
-            "more than one channel not yet implemented, sorry...")
+        raise RuntimeError("more than one channel not yet implemented, sorry...")
 
     for scale_num, scale in enumerate(scales):
 
         try:
             scale = daisy.Coordinate(scale)
         except Exception:
-            scale = daisy.Coordinate((scale,)*chunk_shape.dims())
+            scale = daisy.Coordinate((scale,) * chunk_shape.dims())
 
-        next_voxel_size = prev_array.voxel_size*scale
-        next_total_roi = prev_array.roi.snap_to_grid(
-            next_voxel_size,
-            mode='grow')
-        next_write_size = chunk_shape*next_voxel_size
+        next_voxel_size = prev_array.voxel_size * scale
+        next_total_roi = prev_array.roi.snap_to_grid(next_voxel_size, mode="grow")
+        next_write_size = chunk_shape * next_voxel_size
 
         print("Next voxel size: %s" % (next_voxel_size,))
         print("Next total ROI: %s" % next_total_roi)
         print("Next chunk size: %s" % (next_write_size,))
 
-        next_ds_name = in_ds_name + '/s' + str(scale_num + 1)
+        next_ds_name = in_ds_name + "/s" + str(scale_num + 1)
         print("Preparing %s" % (next_ds_name,))
 
         next_array = daisy.prepare_ds(
@@ -136,7 +131,8 @@ def create_scale_pyramid(in_file, in_ds_name, scales, chunk_shape):
             voxel_size=next_voxel_size,
             write_size=next_write_size,
             dtype=prev_array.dtype,
-            num_channels=num_channels)
+            num_channels=num_channels,
+        )
 
         downscale(prev_array, next_array, scale, next_write_size)
 
@@ -146,32 +142,27 @@ def create_scale_pyramid(in_file, in_ds_name, scales, chunk_shape):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description="Create a scale pyramide for a zarr/N5 container.")
+        description="Create a scale pyramide for a zarr/N5 container."
+    )
 
+    parser.add_argument("--file", "-f", type=str, help="The input container")
+    parser.add_argument("--ds", "-d", type=str, help="The name of the dataset")
     parser.add_argument(
-        '--file',
-        '-f',
-        type=str,
-        help="The input container")
-    parser.add_argument(
-        '--ds',
-        '-d',
-        type=str,
-        help="The name of the dataset")
-    parser.add_argument(
-        '--scales',
-        '-s',
-        nargs='*',
+        "--scales",
+        "-s",
+        nargs="*",
         type=int,
         required=True,
-        help="The downscaling factor between scales")
+        help="The downscaling factor between scales",
+    )
     parser.add_argument(
-        '--chunk_shape',
-        '-c',
-        nargs='*',
+        "--chunk_shape",
+        "-c",
+        nargs="*",
         type=int,
         default=None,
-        help="The size of a chunk in voxels")
+        help="The size of a chunk in voxels",
+    )
 
     args = parser.parse_args()
 
